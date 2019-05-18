6028279db09c68774f7b8743448a3a4f2de8f2c1
vignesh <vicky15011@gmail.com>
2019-05-18 19:23:52 +0530

    Fixed - If multiple request fail, Multiple dialog opens

--- src/app/error-dialog/errordialog.service.ts
+++ src/app/error-dialog/errordialog.service.ts
@@ -4,9 +4,13 @@ import { ErrorDialogComponent } from './errordialog.component';
 
 @Injectable()
 export class ErrorDialogService {
-
+    public isDialogOpen: Boolean = false;
     constructor(public dialog: MatDialog) { }
-    openDialog(data): void {
+    openDialog(data): any {
+        if (this.isDialogOpen) {
+            return false;
+        }
+        this.isDialogOpen = true;
         const dialogRef = this.dialog.open(ErrorDialogComponent, {
             width: '300px',
             data: data
@@ -14,6 +18,7 @@ export class ErrorDialogService {
 
         dialogRef.afterClosed().subscribe(result => {
             console.log('The dialog was closed');
+            this.isDialogOpen = false;
             let animal;
             animal = result;
         });
